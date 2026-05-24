e0ee95e828318747970713d388c1bda86eda7d50
changkm <changkinman06@163.com>
2026-05-24 19:01:01 +0800

    feat(websocket): 实现Netty WebSocket服务器功能
    - 配置Nacos服务发现地址结构
    - 将Netty服务器端口从9000调整为9001
    - 创建MessageInboundHandler处理WebSocket消息
    - 实现NettyServer配置类并集成Spring容器
    - 添加WebSocket协议处理器和消息管道
    - 集成Nacos服务注册功能
    - 配置心跳检测和连接保持机制

--- RealTimeCommunicationService/src/main/java/com/shanyangcode/infinitechat/realtimecommunicationservice/websocket/MessageInboundHandler.java
+++ RealTimeCommunicationService/src/main/java/com/shanyangcode/infinitechat/realtimecommunicationservice/websocket/MessageInboundHandler.java
@@ -1,4 +1,19 @@
 package com.shanyangcode.infinitechat.realtimecommunicationservice.websocket;
 
-public class MessageInboundHandler {
+import cn.hutool.json.JSONUtil;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ChannelHandler.Sharable
+public class MessageInboundHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        log.info("服务端收到了消息：{}", msg);
+
+    }
 }
