492e640321f783276f7a8cec72575d6da7005143
changkm <changkinman06@163.com>
2026-05-30 23:12:04 +0800

    feat(message): 添加离线红包消息体支持
    - 创建 OfflineRedPacketMessageBody 类继承 OfflineMessageBody
    - 添加 redPacketWrapperText 属性存储红包包装文本
    - 使用 Lombok 注解简化代码实现
    - 支持红包消息的离线存储功能

---
+++ OfflineDataStoreService/src/main/java/com/shanyangcode/infinitechat/offlinedatastoreservice/data/offlineMessage/OfflineRedPacketMessageBody.java
@@ -0,0 +1,14 @@
+package com.shanyangcode.infinitechat.offlinedatastoreservice.data.offlineMessage;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+public class OfflineRedPacketMessageBody extends OfflineMessageBody {
+    private String redPacketWrapperText;
+}
