449b40a31ffc06c02b2b530a155be61c61b21d66
changkm <changkinman06@163.com>
2026-06-01 13:28:12 +0800

    feat(datastore): 添加消息数据映射器
    - 创建 MessageMapper 接口继承 MPJBaseMapper
    - 集成 MyBatis Plus 扩展功能
    - 定义消息实体的数据访问层接口

---
+++ OfflineDataStoreService/src/main/java/com/shanyangcode/infinitechat/offlinedatastoreservice/mapper/MessageMapper.java
@@ -0,0 +1,14 @@
+package com.shanyangcode.infinitechat.offlinedatastoreservice.mapper;
+
+import com.github.yulichang.base.MPJBaseMapper;
+import com.shangyangcode.infinitechat.offlinedatastoreservice.model.Message;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MessageMapper extends MPJBaseMapper<Message> {
+
+}
+
+
+
+
