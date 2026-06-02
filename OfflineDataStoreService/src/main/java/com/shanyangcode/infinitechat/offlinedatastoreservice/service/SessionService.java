0b1f81610b62675bec3b16aa2b46b9d2e2af144b
changkm <changkinman06@163.com>
2026-06-02 23:32:00 +0800

    feat(service): 添加会话和用户服务接口
    - 创建SessionService接口继承IService
    - 创建UserService接口继承IService
    - 定义离线数据存储服务的业务层接口
    - 提供基础的数据操作能力扩展点

---
+++ OfflineDataStoreService/src/main/java/com/shanyangcode/infinitechat/offlinedatastoreservice/service/SessionService.java
@@ -0,0 +1,9 @@
+package com.shanyangcode.infinitechat.offlinedatastoreservice.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.shanyangcode.infinitechat.offlinedatastoreservice.model.Session;
+
+public interface SessionService extends IService<Session> {
+
+}
---
+++ OfflineDataStoreService/src/main/java/com/shanyangcode/infinitechat/offlinedatastoreservice/service/UserService.java
@@ -0,0 +1,9 @@
+package com.shanyangcode.infinitechat.offlinedatastoreservice.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.shanyangcode.infinitechat.offlinedatastoreservice.model.User;
+
+public interface UserService extends IService<User> {
+
+}
