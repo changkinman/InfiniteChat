85078dcad5be3d78ddec565c8b1766de816b12e2
changkm <changkinman06@163.com>
2026-06-03 21:17:13 +0800

    fix(service): 修正包名错误

--- OfflineDataStoreService/src/main/java/com/shanyangcode/infinitechat/offlinedatastoreservice/service/Impl/SessionServiceImpl.java
+++ OfflineDataStoreService/src/main/java/com/shanyangcode/infinitechat/offlinedatastoreservice/service/Impl/SessionServiceImpl.java
@@ -2,9 +2,9 @@ package com.shanyangcode.infinitechat.offlinedatastoreservice.service.Impl;
 
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.shangyangcode.infinitechat.offlinedatastoreservice.mapper.SessionMapper;
-import com.shangyangcode.infinitechat.offlinedatastoreservice.model.Session;
-import com.shangyangcode.infinitechat.offlinedatastoreservice.service.SessionService;
+import com.shanyangcode.infinitechat.offlinedatastoreservice.mapper.SessionMapper;
+import com.shanyangcode.infinitechat.offlinedatastoreservice.model.Session;
+import com.shanyangcode.infinitechat.offlinedatastoreservice.service.SessionService;
 import org.springframework.stereotype.Service;
 
 @Service
