7b78600e00574365bf397b5b4bb963455bb77b46
changkm <changkinman06@163.com>
2026-06-17 15:04:03 +0800

    feat(moment): 添加朋友圈点赞服务接口
    - 定义 MomentLikeService 接口继承 IService
    - 添加 createLike 方法用于创建点赞功能
    - 添加 likeMomentResponse 方法处理点赞响应
    - 添加 deleteLikeMoment 方法处理取消点赞响应
    - 包含朋友圈ID和用户ID参数定义
    - 提供点赞和取消点赞的请求响应数据结构

---
+++ MomentService/src/main/java/com/shanyangcode/infinitechat/momentservice/service/MomentLikeService.java
@@ -0,0 +1,28 @@
+package com.shanyangcode.infinitechat.momentservice.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.shanyangcode.infinitechat.momentservice.data.createLike.CreateLikeRequest;
+import com.shanyangcode.infinitechat.momentservice.data.createLike.CreateLikeResponse;
+import com.shanyangcode.infinitechat.momentservice.data.deleteLike.DeleteLikeRequest;
+import com.shanyangcode.infinitechat.momentservice.data.deleteLike.DeleteLikeResponse;
+import com.shanyangcode.infinitechat.momentservice.model.MomentLike;
+
+/**
+ * 朋友圈点赞服务接口
+ * 提供朋友圈点赞相关功能和响应构建
+ */
+public interface MomentLikeService extends IService<MomentLike> {
+    /**
+     * 创建点赞
+     *
+     * @param momentId 朋友圈ID
+     * @param userId 点赞用户ID
+     * @return 创建的点赞ID
+     */
+    Long createLike(Long momentId, Long userId);
+
+    CreateLikeResponse likeMomentResponse(Long momentId, CreateLikeRequest request) throws Exception;
+
+    DeleteLikeResponse deleteLikeMoment(DeleteLikeRequest request);
+}
+
