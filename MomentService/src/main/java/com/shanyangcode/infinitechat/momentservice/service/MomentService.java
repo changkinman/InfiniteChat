a9d677db657dfbfd8d458351816dd434898cd478
changkm <changkinman06@163.com>
2026-06-19 17:48:36 +0800

    feat(moment): 添加动态服务接口定义
    - 定义 MomentService 接口继承 IService
    - 添加创建动态方法 createMoment
    - 添加删除动态方法 deleteMoment
    - 添加获取动态所有者ID方法 getMomentOwnerId
    - 集成创建和删除动态的请求响应数据结构

---
+++ MomentService/src/main/java/com/shanyangcode/infinitechat/momentservice/service/MomentService.java
@@ -0,0 +1,16 @@
+package com.shanyangcode.infinitechat.momentservice.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.shanyangcode.infinitechat.momentservice.data.createMoment.CreateMomentRequest;
+import com.shanyangcode.infinitechat.momentservice.data.createMoment.CreateMomentResponse;
+import com.shanyangcode.infinitechat.momentservice.data.deleteMoment.DeleteMomentRequest;
+import com.shanyangcode.infinitechat.momentservice.data.deleteMoment.DeleteMomentResponse;
+import com.shanyangcode.infinitechat.momentservice.model.Moment;
+
+public interface MomentService extends IService<Moment> {
+    CreateMomentResponse createMoment(CreateMomentRequest request) throws Exception;
+
+    DeleteMomentResponse deleteMoment(DeleteMomentRequest request);
+
+    Long getMomentOwnerId(Long momentId);
+}
